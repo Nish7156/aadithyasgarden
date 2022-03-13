627143cfb6abe9a67de3311c540e2fd3372bfb0e
Nish7156 <shedagenishant40@gmail.com>
2022-03-13 11:43:38 +0530

    new

---
+++ js/routing.js
@@ -0,0 +1,13 @@
+var app = angular.module("myApp", ["ngRoute"]);
+app.config(function($routeProvider) {
+    $routeProvider
+    .when("/red", {
+        templateUrl : "red.htm"
+    })
+    .when("/green", {
+        templateUrl : "green.htm"
+    })
+    .when("/blue", {
+        templateUrl : "blue.htm"
+    });
+});
